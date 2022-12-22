bac567e74deb976bdb40f85efcd43254de788d7b
MiniShark27 <kobifalus@gmail.com>
2022-12-22 00:54:27 -0500

    Test Deployment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'CS 110',
+  title: 'CS 110!',
   tagline: 'Whoa! a Website!',
   url: 'https://CS-110.github.io',
   baseUrl: '/',
